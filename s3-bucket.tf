7ff4b31a81ae6e3f882c4d14ffb297c020907c26
Art K <artem.krt@gmail.com>
2023-03-01 00:17:50 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "my-fuckingXXX-s3-bucket"
+  bucket = "GURAVASKED-s3-bucket"
   acl    = "private"
 
   versioning = {
